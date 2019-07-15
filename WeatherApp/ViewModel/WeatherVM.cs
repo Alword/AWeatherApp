001058f84c0d832430f83128613c48045e010620
Alword <anzer987@yandex.ru>
2019-07-15 16:29:24 +0300

    update

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -11,6 +11,15 @@ namespace WeatherApp.ViewModel
 {
     public class WeatherVM : PropertyChangedBase
     {
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { selectedCity = value; GetWeather(); }
+        }
+
+
         public AccuWeather Weather { get; set; }
 
         private string query;
@@ -27,20 +36,36 @@ namespace WeatherApp.ViewModel
         }
 
         public ObservableCollection<City> Cities { get; set; }
+        public ObservableCollection<DailyForecast> DailyForecasts { get; set; }
 
         public WeatherVM()
         {
             Weather = new AccuWeather();
             Cities = new ObservableCollection<City>();
+            SelectedCity = new City();
+            DailyForecasts = new ObservableCollection<DailyForecast>();
         }
 
         private async void GetCities()
         {
             Cities.Clear();
             List<City> cities = await WeatherApi.GetAutoComplete(Query);
+
+            if (cities is null) return;
             foreach (City city in cities)
             {
                 Cities.Add(city);
+            }
+        }
+
+        private async void GetWeather()
+        {
+            if (selectedCity.Equals(null) || string.IsNullOrEmpty(selectedCity.Key)) return;
+            var weather = await WeatherApi.GetWeatherInformation(selectedCity.Key);
+            DailyForecasts.Clear();
+            foreach (DailyForecast forecast in weather.DailyForecasts)
+            {
+                DailyForecasts.Add(forecast);
             }
         }
     }
